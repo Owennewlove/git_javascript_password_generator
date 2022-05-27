3c4b963351df78eff8b79a8e0c8efa46bec77308
Owen Newlove <owen.newlove@gmail.com>
2022-05-27 17:26:15 -0400

    created function generate password, created a variable size that prompts the user, and created if and else statements that reference the size variable

--- script.js
+++ script.js
@@ -10,5 +10,17 @@ function writePassword() {
 
 }
 
+
+function generatePassword() {
+  var size = prompt("Please enter a value of or between 8 to 128 to define how many characters you would like in your password")
+  if  (size >= 8 && seze <= 128) {
+
+  }
+  else {
+    alert("invalid")
+  }
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
