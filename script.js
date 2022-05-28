ba4d12c4f446f3c23dcc7b30a725ea94b3a017c7
Owen Newlove <owen.newlove@gmail.com>
2022-05-27 20:05:06 -0400

    added a for loop to encase the if statements. Within each if statement I added i++ and used the functions that randomly assign variables within the final password variable so they would be added to the final password, i set the stopping condition in the for loop to be the size entered in the size prompt, and added a console log of the final password to ensure it was giving the right amount of letters but the console log is not really needed anymore

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var number = "0123456789"
 var special = "!@#$%&*?^"
 
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -17,23 +18,23 @@ function writePassword() {
 
 
 function getLowerCase() {
-  return String.fromCharCode(Math.floor(Math.random()*25)+97)
+  return String.fromCharCode(Math.floor(Math.random()*25)+97);
 }
 
 function getUpperCase() {
-  return String.fromCharCode(Math.floor(Math.random()*25)+65)
+  return String.fromCharCode(Math.floor(Math.random()*25)+65);
 }
 
 function getNumericValues() {
-  return String.fromCharCode(Math.floor(Math.random()*9)+48)
+  return String.fromCharCode(Math.floor(Math.random()*9)+48);
 }
 
 function getSpecialCharacters() {
-  return String.fromCharCode(Math.floor(Math.random()*5)+33)
+  return String.fromCharCode(Math.floor(Math.random()*5)+33);
 }
 
 function generatePassword() {
-  var finalpwd = ""
+  var finalpwd = "";
   var size = prompt("Please enter a value of or between 8 to 128 to define how many characters you would like in your password")
   console.log(size)
   if (size >= 8 && size <= 128) {
@@ -46,31 +47,56 @@ function generatePassword() {
     console.log(numericValues)
     var specialCharacters = confirm("include special characters?")
     console.log(specialCharacters)
+
+    for (var i = 0; i < size;) {
+
+      if (lowerCase === true) {
+
+        i++;
+
+        finalpwd = finalpwd += getLowerCase();
+
+
+      } 
+  
+      if (upperCase === true) {
+
+        i++;
+
+        finalpwd = finalpwd += getUpperCase();
+  
+      }
+  
+      if (numericValues === true) {
+
+        i++;
+
+        finalpwd = finalpwd += getNumericValues();
+  
+      }
+  
+      if (specialCharacters === true) {
+
+
+        i++;
+        finalpwd = finalpwd += getSpecialCharacters();
+  
+      }
+
+    }
     
 
-    if (lowerCase === true) {
-
-    } 
-
-    if (upperCase === true) {
-
-    }
-
-    if (numericValues === true) {
-
-    }
-
-    if (specialCharacters === true) {
-
-    }
+    
   }
   else {
     alert("invalid entry")
   }
+  console.log(finalpwd)
 
-  return finalpwd
+  return finalpwd;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
